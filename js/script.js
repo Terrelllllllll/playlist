cc52bda313ecc6cf1e80e5cf213b0f346fe597b8
Terrelllllllll <s_terrell.smith@ousd.org>
2020-01-16 02:21:51 +0000

    array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let playlist = ["High Fashion", "The Box"]
+let playList = ["Look back at it", "The Box", "Life is Good"]
 
 
 
-playlist.forEach(function(songs))
+
+playList.forEach(function(song) {
+  $('#songs').append("<li>" + song + "</li>");
+});
+
+let artistList = ["A boogie with da hoodie", "Roddy Rich", "drake"]
+
+artistList.forEach(function(artist) {
+  $("#artists").append("<li>" + artist + "</li>");
+});
+
+
+let songlength = ["3:00","3:17","5:36"]
+  $("#songlength").append("<li>" + songlength + "</li>");
+});
