ac490e25fd74e5bcb25cf3eac7d331a0e964c66d
Terrelllllllll <s_terrell.smith@ousd.org>
2020-01-23 02:28:06 +0000

    made obeject

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-let playList = ["Look back at it", "The Box", "Life is Good"]
+//let playList = ["Look back at it", "The Box", "Life is Good"]
+let song = {
+songName:"look back at it",
+artistName:"a boogie with da hoodie",
+songLength:"3:00",
+}
+
+let song2= {
+songName:"The box",
+artistName:"Roddy Rich",
+songLength:"3:17",
+}
+
+let song3 = {
+songName:"Life is good",
+artistName:"drake",
+songLength:"5:36",
+}
 
 
 
 
-playList.forEach(function(song) {
-  $('#songs').append("<li>" + song + "</li>");
-});
-
-let artistList = ["A boogie with da hoodie", "Roddy Rich", "drake"]
-
-artistList.forEach(function(artist) {
-  $("#artists").append("<li>" + artist + "</li>");
-});
 
 
-let songlength = ["3:00","3:17","5:36"]
-  $("#songlength").append("<li>" + songlength + "</li>");
-});
+//playList.forEach(function(song) {
+  ////$('#songs').append("<li>" + song + "</li>");
+//});
+
+//let artistList = ["A boogie with da hoodie", "Roddy Rich", "drake"]
+
+//artistList.forEach(function(artist) {
+ //// $("#artists").append("<li>" + artist + "</li>");
+//});
+
+
+//let songlength = ["3:00","3:17","5:36"]
+  //$("#songlength").append("<li>" + songlength + "</li>");
+//});
